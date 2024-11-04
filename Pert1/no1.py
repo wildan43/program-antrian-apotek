6c1c25cd9795022c905b1ecd428812cc1a9e1331
wildan43 <134802946+wildan43@users.noreply.github.com>
2024-11-04 22:10:27 +0700

    Delete Pert1 directory

--- Pert1/no1.py
+++
@@ -1,7 +0,0 @@
-#J0403221025
-#Wildan Holik
-
-hewan = {"kucing":12, "anjing":6, "gajah":23} 
-#Variabel hewan memiliki koleksi data dictionary dengan 3 element dan juga memiliki value
-print(hewan["anjing"])
-#Cetak value dari variabel hewan dengan keys anjing 
