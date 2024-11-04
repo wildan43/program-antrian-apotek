771bc7e4f76357f0b3422633f03e774cdfde100c
wildan43 <134802946+wildan43@users.noreply.github.com>
2024-11-04 22:11:57 +0700

    Delete Pert2 directory

--- Pert2/no1.py
+++
@@ -1,5 +0,0 @@
-#J0403221017
-#M GALUH GUMELAR
-
-fruits = ("apple","banana","cherry")    #Variabel fruits menyimpan beberapa item tuple berupa : apple, banana, chery
-print(len(fruits))                      #Print banyaknya item yang ada di dalam tuple fruits
